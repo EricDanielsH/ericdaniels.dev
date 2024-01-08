c62821fc5c076596b9dae5ed0dcbc77fe6592793
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-01-08 23:41:03 +0000

    Add Project component to index

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import Hero from '@components/Hero'
 import About from '@components/About'
+import Projects from '@components/Projects'
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <About />
+      <Projects />
     </main>
   )
 }
