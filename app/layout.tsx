ff8f024dd4b0567e37c3d04ff9e0442eb26a3395
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-01-08 23:38:02 +0000

    Update metadata title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import Navbar from "@components/Navbar";
 
 export const metadata: Metadata = {
-  title: "Eric Daniels - Software Engineer",
+  title: "Eric",
   description: "Portfolio of Eric Daniels",
 };
 
