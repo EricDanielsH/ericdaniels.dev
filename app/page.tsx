928acbc28b5910daadde5e210d26ace1efe21515
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-01-09 18:52:41 +0000

    Initialise Contact component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
-import Image from 'next/image'
-import Hero from '@components/Hero'
-import About from '@components/About'
-import Projects from '@components/Projects'
+import Image from "next/image";
+import Hero from "@components/Hero";
+import About from "@components/About";
+import Projects from "@components/Projects";
+import Contact from "@components/Contact";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <Hero />
       <About />
       <Projects />
+      <Contact />
     </main>
-  )
+  );
 }
