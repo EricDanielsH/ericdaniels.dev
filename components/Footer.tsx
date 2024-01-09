8843dd5e95b38ce0b6fb181c80730da415aec669
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-01-09 19:33:23 +0000

    Finish footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='bg-slate-50 flex flex-col items-center justify-center py-10 text-xs gap-1'>
+      <div>Built and designed by Eric Daniels</div>
+      <div>All rights reserved &copy;</div>
+    </footer>
   )
 }
 
