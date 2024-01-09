1c325356f7ee833fed8a8b85bd081f858d6f6088
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-01-09 19:26:36 +0000

    Initialise Footer component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "@components/Hero";
 import About from "@components/About";
 import Projects from "@components/Projects";
 import Contact from "@components/Contact";
+import Footer from "@components/Footer";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <About />
       <Projects />
       <Contact />
+      <Footer />
     </main>
   );
 }
