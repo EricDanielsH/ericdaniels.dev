3abf7eb4d0ea89db33ae00a3d5482e813fef903e
EricDanielsH <e.danielshuaman@lancaster.ac.uk>
2024-01-10 08:57:28 +0000

    Remove next/head

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import Head from "next/head"; // Importing the Head component
 import "./globals.css";
 import Navbar from "@components/Navbar";
 
@@ -15,13 +14,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <Head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-        {/* You can add more head elements here as needed */}
-      </Head>
       <body>
         <Navbar />
         {children}
